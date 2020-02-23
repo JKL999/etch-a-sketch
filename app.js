57ea703a478c6e629cf1d6d845866b1c6da94e7c
Tim Zhang <yumouzhang@umass.edu>
2020-02-23 00:20:57 -0500

    Implemented hover effect with single color

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
+const sketch = () => {
+    const container = document.getElementById('container');
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
 
-const container = document.getElementById('container');
+    const createGrid = (rows, cols) => {
+        container.style.setProperty('--grid-rows', rows);
+        container.style.setProperty('--grid-cols', cols);
+        for(c = 0; c < (rows * cols); c++) {
+            let cell = document.createElement('div');
+            // cell.innerText = (c+1);
+            container.appendChild(cell).className = 'grid-item';
+        };
+        const cells = document.querySelectorAll('.grid-item');
+        cells.forEach(cell => {
+            // console.log(cell.innerText);
+            cell.addEventListener('mouseover', function() {
+                this.classList.add('blackOut');
+                // this.classList.add('colorize');
+                // this.style.setProperty('--random-color', randomColor);
+                // console.log(randomColor);
+            });
+        });
+        
 
-const createGrid = (rows, cols) => {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for(c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement('div');
-        /*cell.innerText = (c+1);*/
-        container.appendChild(cell).className = 'grid-item';
     };
+    
+    const clearBtn = document.querySelector('.top-section button');
+        clearBtn.addEventListener('click', () => {
+            clearGrid();
+            let size = prompt('Enter a new grid size', 16);
+            createGrid(size, size);
+        });
+    
+
+    const clearGrid = () => {
+        const cells = document.querySelectorAll('.grid-item');
+        cells.forEach(cell => {
+            cell.classList.remove('blackOut');
+            cell.remove();
+        });
+    
+    };
+    // const colorize = () => {
+    //     this.style.backgroundColor = randomColor;
+    // }
+    
+    
+    
+    
+    createGrid(16,16);
 };
 
-createGrid(16,16);
+
+
+
+sketch();
